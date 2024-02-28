70ca675b58c7828e61eb89f7b1dec0984ae7760f
Mateusz Górka <124613467+mateluszg@users.noreply.github.com>
2024-02-28 15:25:41 +0100

    initial commit

---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import ReactDOM from 'react-dom/client';
+
+import App from './App';
+
+const element = document.getElementById('root');
+
+const root = ReactDOM.createRoot(element as HTMLElement);
+
+root.render(<App />);
